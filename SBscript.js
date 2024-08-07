213fb9355d0183fe2152ce79e1e3fd1f79e25cd3
Harish Mohan <38409115+harishyquichey@users.noreply.github.com>
2024-08-07 13:55:09 -0400

    Mixer
    Added the mixer functionality in its entirety

--- SBscript.js
+++ SBscript.js
@@ -1,240 +1,320 @@
 let isSongPlaying = false;
+let currentAudio = null;
+let currentQueue = [];
+let originalQueue = [];
+let isLoopPlaylistOn = false;
+let isLoopTrackOn = false;
+let isPaused = false;
 
+// Function to update the volume of the current audio
+function updateVolume() {
+  const musicVolume = document.getElementById('musicVolume').value;
+  const volumePercentage = document.getElementById('volumePercentage');
+
+  if (currentAudio) {
+      currentAudio.audio.volume = musicVolume / 100;
+  }
+
+  // Update the volume percentage display
+  volumePercentage.textContent = `${musicVolume}%`;
+}
+
+document.getElementById('musicVolume').addEventListener('input', updateVolume);
+
+
+document.getElementById('musicVolume').addEventListener('input', updateVolume);
 
 function stopAllSound() {
-// Reset the "Now Playing" text
-document.getElementById('currentSound').innerText = "None";
-  if (currentAudio) {
-    currentAudio.pause();
-    currentAudio.currentTime = 0;
-    isSongPlaying = false;
-  }
-}
-
-let currentAudio = null;
-
-// Modify existing playSound function
-function playSound(filename, soundName) {
-  if (currentAudio) {
-    currentAudio.pause();
-    currentAudio.currentTime = 0;
-  }
-  currentAudio = new Audio(filename);
-
-  // Existing event listeners for time update
-  currentAudio.addEventListener('timeupdate', updateAudioProgress);
-  currentAudio.addEventListener('timeupdate', updateTime);
-  
-  // Combine existing logic with new functionality
-  currentAudio.addEventListener('ended', function() {
-    if (currentQueue.length === 0) {
-      document.getElementById('audioProgress').style.width = '0';
-      document.getElementById('currentSound').innerText = 'None';
-      document.getElementById('timeDisplay').innerText = "0:00";
-      isSongPlaying = false;
-    } else {
-      playNextInQueue();
+    document.getElementById('currentSound').innerText = "None";
+    if (currentAudio) {
+        currentAudio.audio.pause();
+        currentAudio.audio.currentTime = 0;
+        isSongPlaying = false;
     }
-  });
-
-  currentAudio.play();
-  isSongPlaying = true;
-  document.getElementById('currentSound').innerText = soundName;
 }
 
-// Function to play the next song in the queue
+function playSound(filename, soundName, menuId) {
+    if (currentAudio) {
+        currentAudio.audio.pause();
+        currentAudio.audio.currentTime = 0;
+    }
+    const audio = new Audio(filename);
+    currentAudio = { audio, menuId };
+
+    // Set initial volume based on the main music slider value
+    updateVolume();
+
+    audio.addEventListener('timeupdate', updateAudioProgress);
+    audio.addEventListener('timeupdate', updateTime);
+
+    audio.addEventListener('ended', function() {
+        if (isLoopTrackOn) {
+            audio.currentTime = 0;
+            audio.play();
+        } else if (currentQueue.length === 0) {
+            if (isLoopPlaylistOn) {
+                currentQueue = [...originalQueue];
+                playNextInQueue();
+            } else {
+                document.getElementById('audioProgress').style.width = '0';
+                document.getElementById('currentSound').innerText = 'None';
+                document.getElementById('timeDisplay').innerText = "0:00";
+                isSongPlaying = false;
+            }
+        } else {
+            playNextInQueue();
+        }
+    });
+
+    audio.play();
+    isSongPlaying = true;
+    isPaused = false;
+    updatePlayPauseButton();
+    document.getElementById('currentSound').innerText = soundName;
+}
+
 function playNextInQueue() {
-  if (currentQueue.length > 0) {
-    const nextSong = currentQueue.shift();
-    playSound(nextSong.filename, nextSong.soundName);
-  }
-}
-
-
-function openMenu(menuId) {
-  closeAllMenus();
-  const menu = document.getElementById(menuId);
-  menu.style.display = 'grid';
-  setTimeout(function() {
-    menu.classList.add('final');
-  }, 100);
-  event.stopPropagation();
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-  // Existing code to stop propagation for sub-menu clicks
-  const subMenus = document.querySelectorAll(".sub-menu");
-  subMenus.forEach((menu) => {
-    menu.addEventListener("click", function(event) {
-      event.stopPropagation();
-    });
-  });
-
-  // Existing code to handle other DOMContentLoaded tasks
-  document.getElementById("nowPlaying").addEventListener("click", function(event) {
-    event.stopPropagation();
-  });
-
-  document.addEventListener("click", function(event) {
-    closeAllMenus();
-  });
-
-  document.getElementById('mainMenu').style.display = 'grid';
-
-  // Add event listeners to buttons of class 'sound-button'
-  const soundButtons = document.querySelectorAll('.sound-button');
-
-  soundButtons.forEach((button) => {
-    button.addEventListener('click', function(event) {
-      event.stopPropagation();  // Prevent the click from closing the menu
-      const submenuId = this.closest('.sub-menu').id;
-      const currentFilename = this.getAttribute('data-filename');
-      populateQueue('sound-button', submenuId, currentFilename);
-    });
-  });
-});
-
-function closeAllMenus() {
-  const subMenus = document.querySelectorAll(".sub-menu.final");
-  subMenus.forEach((menu) => {
-    menu.classList.remove('final');
-    setTimeout(function() {
-      menu.style.display = 'none';
-    }, 1000);
-  });
-}
-
-
-function updateAudioProgress() {
-  const progressBar = document.getElementById('audioProgress');
-  const percentage = (currentAudio.currentTime / currentAudio.duration) * 100;
-  progressBar.style.width = `${percentage}%`;
-}
-
-// Initialize audio progress bar and time
-if (currentAudio) {
-  currentAudio.addEventListener('timeupdate', updateAudioProgress);
-  currentAudio.addEventListener('timeupdate', updateTime);
+    if (currentQueue.length > 0) {
+        const nextTrack = currentQueue.shift();
+        playSound(nextTrack.filename, nextTrack.soundName, nextTrack.menuId);
+    }
 }
 
 function updateAudioProgress() {
-  const progressBar = document.getElementById('audioProgress');
-  const percentage = (currentAudio.currentTime / currentAudio.duration) * 100;
-  progressBar.style.width = `${percentage}%`;
+    const progressBar = document.getElementById('audioProgress');
+    const percentage = (currentAudio.audio.currentTime / currentAudio.audio.duration) * 100;
+    progressBar.style.width = `${percentage}%`;
 }
 
 function updateTime() {
-  const timeDisplay = document.getElementById('timeDisplay');
-  const currentTime = Math.round(currentAudio.currentTime);  // Round to the nearest second
-  const minutes = Math.floor(currentTime / 60);
-  const seconds = Math.floor(currentTime % 60);
-  timeDisplay.textContent = `${minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
+    const timeDisplay = document.getElementById('timeDisplay');
+    const currentTime = Math.round(currentAudio.audio.currentTime);
+    const minutes = Math.floor(currentTime / 60);
+    const seconds = Math.floor(currentTime % 60);
+    timeDisplay.textContent = `${minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
 }
 
-
-// Add scrubbing functionality
+// Scrubbing functionality
 document.getElementById('audioProgressBar').addEventListener('click', function(event) {
-  if (!isSongPlaying) {
-    return; // Do nothing if there's no current song playing
-  }
-  const x = event.pageX - this.offsetLeft;
-  const clickedValue = x * currentAudio.duration / this.offsetWidth;
-  currentAudio.currentTime = clickedValue;
+    if (!isSongPlaying) {
+        return;
+    }
+    const x = event.pageX - this.offsetLeft;
+    const clickedValue = x * currentAudio.audio.duration / this.offsetWidth;
+    currentAudio.audio.currentTime = clickedValue;
 });
 
-// Add variables to track dragging state
-let isDragging = false;  // Flag to check if dragging is active
-
-// Add mousedown event to set isDragging flag to true
+// Dragging state for progress bar
+let isDragging = false;
 document.getElementById('audioProgressBar').addEventListener('mousedown', function(event) {
-  if (!isSongPlaying) {
-    return; // Do nothing if there's no current audio
-  }
-  event.preventDefault(); // Prevent text and other elements from being highlighted
-  isDragging = true;
-  updateProgressBar(event);
-});
-
-// Add mousemove event to update the progress bar if dragging is active
-document.addEventListener('mousemove', function(event) {
-  if (isDragging) {
-    event.preventDefault(); // Prevent text and other elements from being highlighted
+    if (!isSongPlaying) {
+        return;
+    }
+    event.preventDefault();
+    isDragging = true;
     updateProgressBar(event);
-  }
 });
 
-// Add mouseup event to set isDragging flag to false
+document.addEventListener('mousemove', function(event) {
+    if (isDragging) {
+        event.preventDefault();
+        updateProgressBar(event);
+    }
+});
+
 document.addEventListener('mouseup', function() {
-  isDragging = false;
+    isDragging = false;
 });
 
-// Function to update the progress bar and audio currentTime
 function updateProgressBar(event) {
-  if (currentAudio === null) {
-    return; // Do nothing if there's no current audio
-  }
-  
-  const x = event.pageX - document.getElementById('audioProgressBar').offsetLeft;
-  const clickedValue = x * currentAudio.duration / document.getElementById('audioProgressBar').offsetWidth;
-  const percentage = (clickedValue / currentAudio.duration) * 100;
-  currentAudio.currentTime = clickedValue;
-  document.getElementById('audioProgress').style.width = `${percentage}%`;
-}
-
-// Function to randomize an array
-function randomizeArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-}
-
-// Initialize an empty array to hold the current queue of songs
-let currentQueue = [];
-
-// Function to populate the queue based on the submenu and class of clicked button
-function populateQueue(classType, submenuId, currentFilename) {
-  // Initialize or clear the current queue
-  currentQueue = [];
-
-  if (classType === 'sound-button' || classType === 'playlist-button') {
-    const menu = document.getElementById(submenuId);
-    const buttons = menu.querySelectorAll(`.${classType}`);
-    const buttonArray = Array.from(buttons);
-
-    // Shuffle the array
-    for (let i = buttonArray.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [buttonArray[i], buttonArray[j]] = [buttonArray[j], buttonArray[i]];
+    if (currentAudio === null) {
+        return;
     }
 
-    // Populate the queue
-    buttonArray.forEach(button => {
-      const filename = button.getAttribute('data-filename');
-      const soundName = button.getAttribute('data-soundname');
-      if (filename !== currentFilename) {  // Exclude the first song
-        currentQueue.push({ filename, soundName });
+    const x = event.pageX - document.getElementById('audioProgressBar').offsetLeft;
+    const clickedValue = x * currentAudio.audio.duration / document.getElementById('audioProgressBar').offsetWidth;
+    const percentage = (clickedValue / currentAudio.audio.duration) * 100;
+    currentAudio.audio.currentTime = clickedValue;
+    document.getElementById('audioProgress').style.width = `${percentage}%`;
+}
+
+function randomizeArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+function populateQueue(classType, submenuId, currentFilename) {
+  currentQueue = [];
+  originalQueue = [];
+
+  if (submenuId === 'menu14') {
+      // Do not autoplay for menu14, just add the selected sound
+      return;
+  }
+
+  let sourceMenuId = submenuId;
+
+  if (submenuId === 'menu3') {
+      // For menu3, populate from menu2 instead
+      sourceMenuId = 'menu2';
+  }
+
+  if (classType === 'sound-button' || classType === 'playlist-button') {
+      const menu = document.getElementById(sourceMenuId);
+      const buttons = menu.querySelectorAll(`.${classType}`);
+      const buttonArray = Array.from(buttons);
+
+      // Filter out the initial song
+      const filteredButtons = buttonArray.filter(button => button.getAttribute('data-filename') !== currentFilename);
+
+      // Shuffle the remaining songs
+      for (let i = filteredButtons.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [filteredButtons[i], filteredButtons[j]] = [filteredButtons[j], filteredButtons[i]];
       }
-    });
+
+      // Populate the queue with the shuffled list (excluding the initial song)
+      filteredButtons.forEach(button => {
+          const filename = button.getAttribute('data-filename');
+          const soundName = button.getAttribute('data-soundname');
+          currentQueue.push({ filename, soundName, menuId: sourceMenuId });
+      });
+
+      // Populate the original queue with the initial song followed by the shuffled list
+      originalQueue = [{ filename: currentFilename, soundName: document.querySelector(`[data-filename='${currentFilename}']`).getAttribute('data-soundname'), menuId: submenuId }, ...currentQueue];
   }
 }
 
-// Add event listeners to buttons
-document.addEventListener('click', function(event) {
-  const target = event.target;
-  const classType = target.className;
-  const submenuId = target.closest('.sub-menu').id;
 
-  // Populate the queue and start playing
-  populateQueue(classType, submenuId);
+
+document.addEventListener('DOMContentLoaded', function() {
+    const subMenus = document.querySelectorAll(".sub-menu");
+    subMenus.forEach((menu) => {
+        menu.addEventListener("click", function(event) {
+            event.stopPropagation();
+        });
+    });
+
+    document.getElementById("nowPlaying").addEventListener("click", function(event) {
+        event.stopPropagation();
+    });
+
+    document.addEventListener("click", function(event) {
+        closeAllMenus();
+    });
+
+    document.getElementById('mainMenu').style.display = 'grid';
+
+    const soundButtons = document.querySelectorAll('.sound-button');
+    soundButtons.forEach((button) => {
+        button.addEventListener('click', function(event) {
+            event.stopPropagation();
+            const submenuId = this.closest('.sub-menu').id;
+            const currentFilename = this.getAttribute('data-filename');
+            playSound(currentFilename, this.getAttribute('data-soundname'), submenuId);
+            populateQueue('sound-button', submenuId, currentFilename);
+        });
+    });
 });
 
+function closeAllMenus() {
+    const subMenus = document.querySelectorAll(".sub-menu.final");
+    subMenus.forEach((menu) => {
+        menu.classList.remove('final');
+        setTimeout(function() {
+            menu.style.display = 'none';
+        }, 1000);
+    });
+}
+
+function openMenu(menuId) {
+    closeAllMenus();
+    const menu = document.getElementById(menuId);
+    menu.style.display = 'grid';
+    setTimeout(function() {
+        menu.classList.add('final');
+    }, 100);
+    event.stopPropagation();
+}
+
 function openVolumeMixer() {
-  closeAllMenus();  // Close all other menus
-  const menu = document.getElementById('volumeMixer');
-  menu.style.display = 'grid';
-  setTimeout(function() {
-    menu.classList.add('final');
-  }, 100);
-  event.stopPropagation();
+    closeAllMenus();
+    const menu = document.getElementById('volumeMixer');
+    menu.style.display = 'grid';
+    setTimeout(function() {
+        menu.classList.add('final');
+    }, 100);
+    event.stopPropagation();
+}
+
+// Mixer button functionalities
+
+function restartTrack() {
+    if (currentAudio) {
+        currentAudio.audio.currentTime = 0;
+        currentAudio.audio.play();
+    }
+}
+
+function playPauseTrack() {
+    if (!currentAudio) return;
+
+    if (isPaused) {
+        currentAudio.audio.play();
+        isPaused = false;
+    } else {
+        currentAudio.audio.pause();
+        isPaused = true;
+    }
+    updatePlayPauseButton();
+}
+
+function updatePlayPauseButton() {
+    const button = document.querySelector('.mixer-button[onclick="playPauseTrack()"]');
+    if (isPaused) {
+        button.style.backgroundColor = '#7a6bff'; // Purple
+    } else {
+        button.style.backgroundColor = '#1F1F1F'; // Default color
+    }
+}
+
+function skipTrack() {
+    if (isLoopTrackOn) {
+        restartTrack();
+    } else if (isLoopPlaylistOn && currentQueue.length === 0) {
+        currentQueue = [...originalQueue];
+        playNextInQueue();
+    } else if (currentQueue.length > 0) {
+        playNextInQueue();
+    } else {
+        stopAllSound();
+    }
+}
+
+function loopPlaylist() {
+    isLoopPlaylistOn = !isLoopPlaylistOn;
+    isLoopTrackOn = false; // Turn off track loop if playlist loop is on
+    updateLoopButtons();
+}
+
+function loopTrack() {
+    isLoopTrackOn = !isLoopTrackOn;
+    isLoopPlaylistOn = false; // Turn off playlist loop if track loop is on
+    updateLoopButtons();
+}
+
+function stopLoop() {
+    isLoopPlaylistOn = false;
+    isLoopTrackOn = false;
+    updateLoopButtons();
+}
+
+function updateLoopButtons() {
+    const loopPlaylistButton = document.querySelector('.mixer-button[onclick="loopPlaylist()"]');
+    const loopTrackButton = document.querySelector('.mixer-button[onclick="loopTrack()"]');
+
+    loopPlaylistButton.style.backgroundColor = isLoopPlaylistOn ? '#7a6bff' : '#1F1F1F'; // Purple or default
+    loopTrackButton.style.backgroundColor = isLoopTrackOn ? '#7a6bff' : '#1F1F1F'; // Purple or default
 }
